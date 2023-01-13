ab0ac6b5457102a89e092d3a7d0d1563ca010131
Victor Martins <32188485+vkunssec@users.noreply.github.com>
2023-01-13 10:40:12 -0300

    choore: alteracao do projeto para assincrono

--- redis/client.js
+++ redis/client.js
@@ -1,31 +1,35 @@
 const redis = require("redis");
 
-// Creates a new Redis client
-// If REDIS_HOST is not set, the default host is localhost
-// If REDIS_PORT is not set, the default port is 6379
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT  
-});
-
-redisClient.on("error", function(err) {
-    console.log("Error " + err);
-});
-
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hSet("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hSet("species", "robotocat", "Cat and Robot", redis.print);
-// Gets all fields in "species" key
-
-redisClient.hKeys("species", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
+(async () => {
+    // Creates a new Redis client
+    // If REDIS_HOST is not set, the default host is localhost
+    // If REDIS_PORT is not set, the default port is 6379
+    const redisClient = redis.createClient({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT  
     });
-    // redisClient.quit();
-});
+    
+    redisClient.on("error", function(err) {
+        console.log("Error " + err);
+    });
+    
+    await redisClient.connect();
+    
+    // Sets the key "octocat" to a value of "Mona the octocat"
+    await redisClient.set("octocat", "Mona the Octocat", redis.print);
+    // Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
+    await redisClient.hSet("species", "octocat", "Cat and Octopus", redis.print);
+    // Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
+    await redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus", redis.print);
+    // Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
+    await redisClient.hSet("species", "robotocat", "Cat and Robot", redis.print);
+    // Gets all fields in "species" key
+    
+    redisClient.hKeys("species", function (err, replies) {
+        console.log(replies.length + " replies:");
+        replies.forEach(function (reply, i) {
+            console.log("    " + i + ": " + reply);
+        });
+        redisClient.quit();
+    });
+})();
